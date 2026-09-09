a13708a1b8b968a9c0c6505486fd708d8ed2d3d7
Pablo <pablo.gonzalez0328@gmail.com>
2026-09-08 19:40:14 -0500

    seeds rb configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Book.create(title: "The Great Gatsby", author: "F. Scott Fitzgerald")
+Book.create(title: "1984", author: "George Orwell")
+Book.create(title: "The Hobbit", author: "J.R.R. Tolkien")
+Book.create(title: "Dune", author: "Frank Herbert")
+Book.create(title: "Frankenstein", author: "Mary Shelley")
